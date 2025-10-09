dd3084f2b1be626fbf1f9ba9506225c86d4d8953
RIN00001 <hamerboy37@gmail.com>
2025-10-09 19:47:14 +0700

    BOotsrap test

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-primary">Bootstrap + Vite Works!</h1>
+        <button class="btn btn-success">Test Button</button>
+    </div>
+</body>
+</html>
