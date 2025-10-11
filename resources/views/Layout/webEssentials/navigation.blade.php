0fcfb45949c239313aaa0e586c181e89ef913f89
RIN00001 <hamerboy37@gmail.com>
2025-10-11 16:16:32 +0700

    Navbar Dynamic Update

--- resources/views/Layout/webEssentials/navigation.blade.php
+++ resources/views/Layout/webEssentials/navigation.blade.php
@@ -16,20 +16,28 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+          <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" 
+             {{ request()->is('/') ? 'aria-current=page' : '' }} 
+             href="/">Beranda</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/tentang">Tentang</a>
+          <a class="nav-link {{ request()->is('tentang') ? 'active' : '' }}" 
+             {{ request()->is('tentang') ? 'aria-current=page' : '' }} 
+             href="/tentang">Tentang</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/program">Program</a>
+          <a class="nav-link {{ request()->is('program') ? 'active' : '' }}" 
+             {{ request()->is('program') ? 'aria-current=page' : '' }} 
+             href="/program">Program</a>
         </li>
           <li class="nav-item">
-          <a class="nav-link" href="/berita">Berita</a>
+          <a class="nav-link {{ request()->is('berita*') ? 'active' : '' }}" 
+             {{ request()->is('berita*') ? 'aria-current=page' : '' }} 
+             href="/berita">Berita</a>
         </li>
         <li class="nav-item dropdown">
           <a
-            class="nav-link dropdown-toggle"
+            class="nav-link dropdown-toggle {{ request()->is('kontak') ? 'active' : '' }}"
             href="#"
             role="button"
             data-bs-toggle="dropdown"
@@ -40,7 +48,7 @@
             <li><a class="dropdown-item" href="#">Mahasiswa Baru</a></li>
             <li><a class="dropdown-item" href="#">Beasiswa</a></li>
             <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Kontak</a></li>
+            <li><a class="dropdown-item {{ request()->is('kontak') ? 'active' : '' }}" href="/kontak">Kontak</a></li>
           </ul>
         </li>
       </ul>
